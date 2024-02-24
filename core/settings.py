9f4cc6d69a0cebdb7e5a9e3b0ce79aafe6aa774a
Anderson <jelow0413@gmail.com>
2024-02-23 19:44:10 -0500

    Update database settings to use MySQL

--- core/settings.py
+++ core/settings.py
@@ -71,11 +71,14 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME':'railway',
+        'USER':'root',
+        'PASSWORD':'cHg2BDDcd3f5a2CD4CbBbc26B4DGCHG6',
+        'HOST':'monorail.proxy.rlwy.net',
+        'PORT':'51904',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
